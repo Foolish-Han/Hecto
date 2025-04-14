99ff06e17ca0fc54ea209089dfbcfc06eaf17aa2
Foolish-Han <nmdqbcmz@gmail.com>
2025-04-14 17:16:02 +0800

    A Basic Line Viewer

--- src/editor.rs
+++ src/editor.rs
@@ -4,6 +4,7 @@ use crossterm::event::{
     KeyCode::{self},
     KeyEvent, KeyEventKind, KeyModifiers, read,
 };
+use std::env;
 use std::io::Error;
 
 mod terminal;
@@ -28,9 +29,17 @@ pub struct Editor {
 impl Editor {
     pub fn run(&mut self) {
         Terminal::initialize().unwrap();
+        self.handle_args();
         let result = self.repl();
         Terminal::terminate().unwrap();
         result.unwrap();
+    }
+
+    fn handle_args(&mut self) {
+        let args: Vec<String> = env::args().collect();
+        if let Some(file_name) = args.get(1) {
+            self.view.load(file_name);
+        }
     }
 
     pub fn repl(&mut self) -> Result<(), Error> {
