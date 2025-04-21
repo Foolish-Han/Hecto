9104777c09c4e50ac4ae78848c969f07a16e8ac0
Foolish-Han <nmdqbcmz@gmail.com>
2025-04-21 12:10:43 +0800

    Snap Cursor to End of Line

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -2,6 +2,7 @@ use super::{
     editorcommands::{Direction, EditorCommand},
     terminal::{Position, Size, Terminal},
 };
+use std::cmp::min;
 mod buffer;
 use buffer::Buffer;
 mod line;
@@ -70,19 +71,48 @@ impl View {
         self.location.subtrace(&self.scroll_offset).into()
     }
 
+    // clippy::arithmetic_side_effects: This function performs arithmetic calculations
+    // after explicitly checking that the target value will be within bounds.
+    #[allow(clippy::arithmetic_side_effects)]
     fn move_text_location(&mut self, direction: &Direction) {
         let Location { mut x, mut y } = self.location;
-        let Size { height, width } = self.size;
+        let Size { height, .. } = self.size;
+        // This match moves the positon, but does not check for all boundaries.
+        // The final boundary checking happens after the match statement.
         match direction {
             Direction::Up => y = y.saturating_sub(1),
             Direction::Down => y = y.saturating_add(1),
-            Direction::Left => x = x.saturating_sub(1),
-            Direction::Right => x = x.saturating_add(1),
+            Direction::Left => {
+                if x > 0 {
+                    x -= 1;
+                } else if y > 0 {
+                    y -= 1;
+                    x = self.buffer.lines.get(y).map_or(0, |line| line.len());
+                }
+            }
+            Direction::Right => {
+                let width = self.buffer.lines.get(y).map_or(0, |line| line.len());
+                if x < width {
+                    x = x + 1;
+                } else {
+                    y = y.saturating_add(1);
+                    x = 0;
+                }
+            }
             Direction::Home => x = 0,
-            Direction::End => x = width.saturating_sub(1),
-            Direction::PageUp => y = 0,
-            Direction::PageDown => y = height.saturating_sub(1),
+            Direction::End => x = self.buffer.lines.get(y).map_or(0, |line| line.len()),
+            Direction::PageUp => y = y.saturating_sub(height).saturating_sub(1),
+            Direction::PageDown => y = y.saturating_add(height).saturating_sub(1),
         }
+        //snap x to valid position
+        x = self
+            .buffer
+            .lines
+            .get(y)
+            .map_or(0, |line| min(x, line.len()));
+
+        //snap y to valid position
+        y = min(y, self.buffer.lines.len());
         self.location = Location { x, y };
         self.scroll_location_into_view();
     }
